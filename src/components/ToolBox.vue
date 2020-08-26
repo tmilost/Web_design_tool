0713aac31174e7094569cdf299e9a5f80803a03f
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-08-26 17:22:46 +0200

    add simple box

---
+++ src/components/ToolBox.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="toolBox">
+    <div class="toolboxSize">
+
+
+<el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>ToolBox</span>
+        <el-button style="float: right; padding: 3px 4px 3px 4px"  size="mini">X</el-button>
+  </div>
+  <div v-for="o in 4" :key="o" class="text item">
+    {{'List item ' + o }}
+  </div>
+</el-card>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.toolboxSize{
+  width: 1250px;
+  align-items: left;
+}
+.text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 220px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <ToolBox />
+        <!-- <ToolBox msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ToolBox from '@/components/ToolBox.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ToolBox
   }
 }
 </script>
