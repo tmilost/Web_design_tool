775075ee08403f9abaafae56ec59164f0cc2436a
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-09-01 17:03:21 +0200

    values and search

--- src/components/ToolBox.vue
+++ src/components/ToolBox.vue
@@ -1,56 +1,127 @@
 <template>
   <div class="toolBox">
     <div class="toolboxSize">
-
-
-<el-card class="box-card">
-  <div slot="header" class="clearfix">
-    <span>ToolBox</span>
-        <el-button style="float: right; padding: 3px 4px 3px 4px"  size="mini">X</el-button>
-  </div>
-  <div v-for="o in 4" :key="o" class="text item">
-    {{'List item ' + o }}
-  </div>
-</el-card>
-
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>ToolBox</span>
+          <el-button style="float: right; padding: 3px 4px 3px 4px" size="mini"
+            >X</el-button
+          >
+        </div>
+        <div class="search">
+          <el-input placeholder="Filter keyword" v-model="filterText">
+          </el-input>
+        </div>
+        <div class="tree">
+          <el-tree
+          empty-text="No component were found"
+            class="filter-tree"
+            :data="components"
+            :props="defaultProps"
+            default-expand-all
+            :filter-node-method="filterNode"
+            ref="tree2"
+          >
+          </el-tree>
+        </div>
+      </el-card>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  // name: 'HelloWorld',
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val);
+    },
+  },
+
+  methods: {
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
+  },
+  data: function() {
+    return {
+       filterText: '',
+      components: [
+        {
+          label: "Controls",
+          children: [
+            {
+              label: "Button",
+            },
+              {
+              label: "Split Button",
+            },
+            {
+              label: "Button Group",
+            },
+            {
+              label: "Button Toolbar",
+            },
+            {
+              label: "Dropdown",
+            },
+             {
+              label: "Link",
+            },
+          ],
+        },
+        {
+          label: "Level one 2",
+          children: [
+            {
+              label: "Level two 2-1",
+            },
+          ],
+        },
+        {
+          label: "Level one 3",
+          children: [
+            {
+              label: "Level two 3-1",
+            },
+          ],
+        },
+      ],
+    };
+  },
   // props: {
   //   msg: String
   // }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.toolboxSize{
+.toolboxSize {
   width: 1250px;
   align-items: left;
 }
 .text {
-    font-size: 14px;
-  }
+  font-size: 14px;
+}
 
-  .item {
-    margin-bottom: 18px;
-  }
+.item {
+  margin-bottom: 18px;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
 
-  .box-card {
-    width: 220px;
-  }
+.box-card {
+  width: 220px;
+}
+.tree{
+  margin-top: 25px;
+}
 </style>
