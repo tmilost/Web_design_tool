5649571df1bd3f00d06c74716eeef9ce00867852
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-09-05 23:36:25 +0200

    format

--- src/components/canvasTheme.vue
+++ src/components/canvasTheme.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="mainDiv">
-      <header>
-<div class="headerText">
-Bush plane
-</div>
-    
-      </header>
+    <header>
+      <div class="headerText">
+        Bush plane
+      </div>
+    </header>
     <div>
-      <img class="planeImg" src="@/assets/Trent_Palmer_Bush_Plane.jpg" alt="Bush_Plane" />
+      <img
+        class="planeImg"
+        src="@/assets/Trent_Palmer_Bush_Plane.jpg"
+        alt="Bush_Plane"
+      />
     </div>
   </div>
 </template>
 
 <style>
-header{
-    background-color: #aacbcd;
-    height: 56px;;
+header {
+  background-color: #aacbcd;
+  height: 56px;
 }
-.planeImg{
-    width: 100%;
-    height: 20%;
+.planeImg {
+  width: 100%;
+  height: 20%;
 }
-.headerText{
-    color: white;
-    padding: 15px 0px 0px 20px;
+.headerText {
+  color: white;
+  padding: 15px 0px 0px 20px;
 }
 </style>
