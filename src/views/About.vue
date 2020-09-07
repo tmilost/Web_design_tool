afc53c07a7dd27b81b06bdf87d52a064d8494ca9
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-09-07 23:50:47 +0200

    //

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>
+      A powerful Web app for creating responsive components using the Vue.js
+      (2.x) framework.
+    </h2>
   </div>
 </template>
