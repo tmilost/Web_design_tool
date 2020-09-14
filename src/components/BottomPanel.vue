62e870be284d9e08341d3ccdc058886d3c4b1ffe
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-09-15 00:52:49 +0200

    bottom panel postition set

--- src/components/BottomPanel.vue
+++ src/components/BottomPanel.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="bottomPanel">
-adasdasd
-    </div>
+  <div class="bottomPanel">
+    adasdasd
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
+.bottomPanel {
+  height: 20vh;
+  width: calc(100vw - 229px - 250px);
+  background-color: blue;
+  position: fixed;
+  left: 250px;
+  bottom: 0px;
+}
 </style>
