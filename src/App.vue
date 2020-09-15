10ddf19c9fe77cc71d14ad2a6d206427e874355f
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-09-15 03:57:38 +0200

    scrollbar customization

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,15 @@
 body {
   margin: 0;
 }
+::-webkit-scrollbar {
+  height: 6px;
+  width: 6px;
+  background: rgba(0, 0, 0, 0.2);
+}
+::-webkit-scrollbar-corner {
+  display: none;
+}
+::-webkit-scrollbar-thumb {
+  background: #677073;
+}
 </style>
